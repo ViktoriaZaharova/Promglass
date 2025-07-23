c01b8367ba39026637d1d619c202163c425ab32c
Viktoria Zaharova <zaharova181114@gmail.com>
2025-07-24 01:19:18 +0300

    + 2 section

--- js/common.js
+++ js/common.js
@@ -1,2 +1,14 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+// Fancybox
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
+
+// sliders
+$('.history-company-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+});
