e689e9d95fc2e8249bc7f6809fdd0a62ec52b618
Viktoria Zaharova <zaharova181114@gmail.com>
2025-07-25 14:48:04 +0300

    home 2/3

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 // Fancybox
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
+  // Your custom options
 });
 
 // sliders
@@ -10,5 +10,21 @@ $('.history-company-slider').slick({
   slidesToShow: 1,
   fade: true,
   prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+});
+
+$('.home-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  appendArrow: 'home-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+});
+//auto counter total//
+let homeSlider = $('.home-slider');
+
+$('.counter-slide__default').text("/ " + homeSlider.slick("getSlick").slideCount);
+
+homeSlider.on('afterChange', function (event, slick, currentSlide) {
+  $(".counter-slide__cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
 });
