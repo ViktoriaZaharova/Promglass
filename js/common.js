3a2b746dd022a01e1bc1ba1412c241995a596c34
Viktoria Zaharova <zaharova181114@gmail.com>
2025-08-09 23:59:32 +0300

    article page

--- js/common.js
+++ js/common.js
@@ -20,6 +20,27 @@ $('.home-slider').slick({
   prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
   nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
 });
+
+$('.article-slider').slick({
+  slidesToShow: 3,
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
+
 //auto counter total//
 let homeSlider = $('.home-slider');
 
