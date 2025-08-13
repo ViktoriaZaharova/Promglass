776642e8fd0dd984d921bc8fb0b86adcf01b4d39
Viktoria Zaharova <zaharova181114@gmail.com>
2025-08-14 01:35:55 +0300

    manufacturer

--- js/common.js
+++ js/common.js
@@ -69,6 +69,34 @@ $('.package-slider').slick({
   ]
 });
 
+$('.equipment-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
+$('.quality-slider').slick({
+  slidesToShow: 3,
+  appendArrows: '.quality-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
+
 $(function () {
   $('.advantages-box').each(function () {
     let $this = $(this);
